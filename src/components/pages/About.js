cf6c08f1f9560c0fa75217d5cdc83467e5d06d46
Francisco <francisco.caldera2015@gmail.com>
2023-06-05 21:01:27 -0500

    editing about and added css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
+
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <div class="row">
+        <div class="col s12 m5">
+          <div class="card-panel">
+            <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
